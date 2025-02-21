e13bcdd94702904e6652091195e02bd302447d4d
EthanFOISSOTTE <ethan.foissotte@epitech.eu>
2025-02-21 13:29:10 +0100

    [ADD]-Début du script du bouton de tri de niveau de connaissance

---
+++ Pages/Scripts/bouton_trie_connaissance.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const BtnTri = document.querySelector(".tri_connaissance");
+    const allSkills = document.querySelectorAll(".skill div > div:not()");
+});
