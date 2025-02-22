54b1214280265c26353475256eb6ea1ec3299b0b
EthanFOISSOTTE <ethan.foissotte@epitech.eu>
2025-02-22 15:56:14 +0100

    [EDIT]-Script de tri de connaissances selon le niveau

--- Pages/Scripts/bouton_trie_connaissance.js
+++ Pages/Scripts/bouton_trie_connaissance.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const BtnTri = document.querySelector(".tri_connaissance");
-    const allSkills = document.querySelectorAll(".skill div > div:not()");
+    const btnTri = document.querySelector(".tri_connaissance");
+    const allSkills = document.querySelectorAll(".skill div > div:not(.categorie_autre, .categorie_backend, .categorie_database, .categorie_devops, .categorie_dev_mobile, .categorie_programmation, .categorie_frontend, .categorie_game_engine, .categorie_ia)");
+
+    let niveau = ["Tout", "Débutant", "Intermédiaire", "Avancé"];
+    let index = 0;
+
+    btnTri.addEventListener("click", function () {
+        index = (index + 1) % niveau.length;
+        let niveauSelectionne = niveau[index];
+        btnTri.textContent = `Connaissance : ${niveauSelectionne}`;
+
+        allSkills.forEach(skill => {
+            let niveau = skill.querySelector(".niveau")?.textContent;
+
+            if (niveauSelectionne === "Tout" || (niveau && niveau.includes(niveauSelectionne))) {
+                skill.style.display = "flex";
+            } else {
+                skill.style.display = "none";
+            }
+        });
+    });
 });
